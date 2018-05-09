738dade091a526dbfe1348454cf7c05dd8884060
Navish <navish45@gmail.com>
2018-05-09 20:17:29 +0300

    git.ignore fix

--- api/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-
-    'secret': 'NavishIsBae',
-    'database': 'mongodb://localhost/aboutme'
-    //'database': 'mongodb://noder:noderauth&54;proximus.modulusmongo.net:27017/so9pojyN'
-
-};
