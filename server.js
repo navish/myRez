66a90c19464027c22fba5d20748557963b53dfae
Navish <navish45@gmail.com>
2018-05-09 15:21:45 +0300

    Updated npm

--- server.js
+++ server.js
@@ -6,32 +6,21 @@ var app = express();
 var session = require('express-session');
 
 var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
-var config = require('./dbconfig.js'); // get our config file
+var config = require('./dbconfig.js'); // get ur config file
 var mongodb = require('mongodb');
 
 mongoClient = mongodb.MongoClient,
 ObjectID = mongodb.ObjectID; // Used in API endpoints
-const MongoStore = require('connect-mongo')(session);
 
 app.use(bodyParser.json());
-app.set('port', process.env.PORT || 8080);
-app.use(cors()); // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
-app.use(express.static("www")); // Our Ionic app build is in the www folder (kept up-to-date by the Ionic CLI using 'ionic serve')
+app.set('port', process.env.PORT || 8000);
+app.use(cors()); 
+app.use(express.static("www"));
 
-//var MONGODB_URI = process.env.MONGODB_URI;
-var MONGODB_URI = 'mongodb://localhost/aboutme'
+var MONGODB_URI = config.database;
 app.set('superSecret', config.secret); // secret variable
 
-app.use(session({
-    secret:'secret',
-    resave:false,
-    saveUninitialized: true,
-    store: new MongoStore({
-        url: MONGODB_URI,
-        ttl: 1 * 1 * 60,
-        autoRemove: 'native'
-    })
-}))
+
 
 // Initialize database connection and then start the server.
 mongoClient.connect(MONGODB_URI, function (err, database) {
@@ -49,11 +38,42 @@ console.log("You just made the app listen to the db", app.get('port'));
 function handleError(res, errMsg){
     return res.status(400).send(errMsg);
 }
-// About Me API Routes Will Go Below
 
-//Get User by username
+// API ROUTES //
+
+    //Login
+    app.post("/api/sessions/create", function(req,res) {
+        let user = req.body;
+        db.collection("users").find({ $and : [{"email": user.username }, {"password": user.password }] }).toArray(function(err,doc){
+            if (err || !doc || doc.length === 0) {
+                handleError(res,"Failed to login");
+            } else {
+                // create a token with your own payload, don't pass in the entire user since that has the password
+                const payload = {
+                    userAppId: user.username 
+                };
+                var token = jwt.sign(payload, app.get('superSecret'), {
+                    expiresIn: 1440 // expires in 24 hours
+                });
+                
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'logged in',
+                    token: token,
+                });
+                
+            }
+        })
+    })
+
+    //Register
+    app.post("/api/user/register/", function(){
+        let newUser = req.body.newUser
+    })
+    //Get User by username
     app.post("/api/user/", function(req, res) {
-        let user = req.body.username
+        let user = req.body.username;
         db.collection("users").find({"email": user.email}).toArray(function(err, doc) {
             if (err) {
                 handleError(res, "Failed to get the user by username");
@@ -62,40 +82,25 @@ function handleError(res, errMsg){
             }
         });
     });
-
-
-//Login
-app.post("/api/sessions/create", function(req,res) {
-    let user = req.body;
-    db.collection("users").find({ $and : [{"email": user.username }, {"password": user.password }] }).toArray(function(err,doc){
-        if (err || !doc || doc.length === 0) {
-            handleError(res,"Failed to login");
-        } else {
-            // create a token with your own payload, don't pass in the entire user since that has the password
-            const payload = {
-                userAppId: user.username 
-            };
-            var token = jwt.sign(payload, app.get('superSecret'), {
-                expiresIn: 1440 // expires in 24 hours
-              });
-            
-              // return the information including token as JSON
-              res.json({
-                success: true,
-                message: 'logged in',
-                token: token,
-              });
-            
-        }
-    })
-})
-//Get all users
+    //Get all users
     app.get("/api/users/", function(req, res) {
         db.collection("users").find({}).toArray(function(err, doc) {
             if (err) {
             handleError(res, "Failed to get users");
             } else {
             res.status(200).json(doc);
+            }
+        });
+    });
+
+
+    //Get all resolution lists
+    app.get("/api/resolution_lists/", function(req, res) {
+        db.collection("resLists").find({}).toArray(function(err, doc) {
+            if (err) {
+            handleError(res, "Failed to get Lists");
+            } else {
+            res.json(doc);
             }
         });
     });
